333168e9c2799fc21d862a2a9480a9f80a96f773
Christian Barreto <chrbarreto@gmail.com>
2017-10-15 01:21:24 -0200

    Loops
    part 2

---
+++ Loops.r
@@ -0,0 +1,15 @@
+#### 4. LOOPS ####
+
+# 4.1 WHILE
+count <- 0
+while (count <= 10){
+  print(count * count)
+  count <- count + 1
+}
+
+# 4.2 FOR loop
+for (i in 0:10) {
+  print(i * i)
+}
+
+# for both of them, the result is going to be 0 1 4 9 16 25 36 49 64 81 100
